ef0bc8f54bf4fd92c40acd79d43997acaeaeaf00
Seon Mi Kim <tjsalsun@gmail.com>
2022-11-18 03:10:24 +0900

    props userObj 추가

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,13 @@ import{getAuth, onAuthStateChanged} from "firebase/auth"
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(()=>{
     const auth = getAuth();
     onAuthStateChanged(auth,(user)=>{
       if(user){
         setIsLoggedIn(true);
+        setUserObj(user)
         const uid = user.uid
       }else{
         setIsLoggedIn(false);
@@ -22,7 +24,7 @@ function App() {
 
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn}/>
+      <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
       <footer>&copy; Nwitter{new Date().getFullYear()} Nwitter</footer>
     </>
   )
