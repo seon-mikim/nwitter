03807624397d9cff61cb0efcbe33fabc4f249e25
Seon Mi Kim <tjsalsun@gmail.com>
2022-11-18 03:11:43 +0900

    userObj props 전달및 이벤트 핸들러 수정

--- src/router/Home.js
+++ src/router/Home.js
@@ -1,23 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "fbase";
+import{addDoc, collection, getDocs, query, onSnapshot, orderBy, snapshotEqual } from "firebase/firestore"
 
-const Home = () => {
+const Home = ({userObj}) => {
+    console.log(userObj)
     const [nweet, setNweet] = useState("");
-    const onSubmit = (event) => {
+    const [nweets, setNweets] = useState([]);
+ 
+
+    useEffect(()=>{
+        const q = query(collection(dbService,"nweets"),orderBy("createdAt","desc"));
+        onSnapshot(q,(snapshot)=>{
+            const nweetArr = snapshot.docs.map((document)=>({
+                id: document.id,
+                ...document.data(),
+            }));
+            setNweets(nweetArr);
+        })
+    }, []);
+    
+    const onSubmit = async(event) => {
         event.preventDefault();
+        try{
+            const docRef = await addDoc(collection(dbService, "nweets"),{
+                text:nweet,
+                createdAt:Date.now(),
+                createId:userObj.uid,
+            });
+            
+        } catch (error){
+            console.error("Error adding document:", error);
+        }
+        setNweet("");
     };
 
-    const onChange = (event) => {
-        const {
-            target: {value}
-        } = event;
+    const onChange = ( {target: {value}} ) => {
+      
         setNweet(value)
     };
+
+    
     return(
         <div>
             <form onSubmit={onSubmit}>
                 <input onChange={onChange} value={nweet} type="text" placeholder="What's your mind?" maxLength={120}/>
                 <input type="submit" value="Nweet"/>
             </form>
+            <div>
+                {nweets.map((nweet)=>( 
+                    <div key={nweet.id}>
+                        <h4>{nweet.text}</h4>
+                        <p>{new Date(nweet.createdAt).toLocaleString()}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
